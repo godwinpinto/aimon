4a1e2f987e4deccb533b6af0f96a23b5fec9a885
Godwin Pinto <godwin.pinto86@gmail.com>
2023-07-22 14:16:45 +0530

    feat: ingest api

--- extension/src/components/popups/LogPasteEvents.svelte
+++ extension/src/components/popups/LogPasteEvents.svelte
@@ -7,34 +7,51 @@
     import type { TAlertProps } from "../types/AlertPropsTypes";
     import type { TPolicyMessage } from "../helpers/PolicyHelper";
 
+    let isCopyTextEmitted: boolean = false;
 
-    let isCopyTextEmitted:boolean=false;
+    // Define an empty object to suppress navigator.clipboard
+const emptyClipboard = {
+  writeText: async () => {},
+  readText: async () => {},
+  // Add other methods you want to suppress (e.g., write, read, etc.)
+   write: async () => {},
+   read: async () => {},
+  // ...
+};
 
+// Suppress navigator.clipboard by redefining it as an empty object
+Object.defineProperty(navigator, 'clipboard', {
+  value: emptyClipboard,
+});
 
-    jQuery("body").on("click", function(event) {
-      if (jQuery(event.target).is("button")) {
-        var buttonText = jQuery(event.target).text();
-        if(buttonText.toLowerCase().includes("copy")){
-            isCopyTextEmitted=true;
-            console.log('Clicked button text:', buttonText);
-            let hiddenInput = jQuery('<input>').attr('type', 'text').attr('id', 'hidden-input');
-            jQuery('body').append(hiddenInput);
-            jQuery('#hidden-input').trigger("focus");
-            document.execCommand('paste');
-            let clipboardData = jQuery('#hidden-input').val();
-            console.log('Clipboard data:', clipboardData);
-            hiddenInput.remove();
+    jQuery("body").on("click", function (event) {
+        if (jQuery(event.target).is("button")) {
+            let buttonText = jQuery(event.target).text();
+            console.log("buttonText", buttonText);
+            if (buttonText.toLowerCase().includes("copy")) {
+                isCopyTextEmitted = true;
+                console.log("Clicked button text:", buttonText);
+                let hiddenInput = jQuery("<input>")
+                    .attr("type", "text")
+                    .attr("id", "hidden-input");
+                jQuery("body").append(hiddenInput);
+                jQuery("#hidden-input").trigger("focus");
+                document.execCommand("paste");
+                let clipboardData = jQuery("#hidden-input").val();
+                console.log("Clipboard data:", clipboardData);
+                hiddenInput.remove();
+            }
         }
-      }
     });
 
-
-
     jQuery(document).on("paste", function (e) {
-        if(!isCopyTextEmitted){
-            console.log("paste function",e.originalEvent.clipboardData.getData("text"));
-        }else{
-            isCopyTextEmitted=false;
+        if (!isCopyTextEmitted) {
+            console.log(
+                "paste function",
+                e.originalEvent.clipboardData.getData("text")
+            );
+        } else {
+            isCopyTextEmitted = false;
         }
         return true;
     });
@@ -51,4 +68,3 @@
         return true;
     });
 </script>
-
