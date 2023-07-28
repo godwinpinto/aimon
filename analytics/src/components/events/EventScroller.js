c1b3792666c77695deea9b1448063b29887c8165
Godwin Pinto <godwin.pinto86@gmail.com>
2023-07-28 09:21:52 +0530

    feat: analytics api integration

--- analytics/src/components/events/EventScroller.js
+++ analytics/src/components/events/EventScroller.js
@@ -4,7 +4,7 @@ import './eventScroller.css';
 import { Box, Typography, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
 import { FlipMove } from 'react-flip-move';
-
+import axios from 'axios';
 
 const EventScroller = () => {
 const theme = useTheme();
@@ -25,12 +25,46 @@ const colors = tokens(theme.palette.mode);
     });
   };
 
+let holdData=[];
+let rowId=1;
+const eventAdder = async () => {
+  if(holdData.length>0){
+    let temp=holdData.shift();
+    temp.id=rowId++;
+    console.log("temp",temp)
+    setEvents(prevEvents => {
+      const updatedEvents = [temp,...prevEvents ];
+      return updatedEvents.slice(0, 10); // Ensure the list has a maximum of 10 records
+    });
+  }else{
+    console.log("empty")
+  }
+}
+  const fetchEventsFromApi = async () => {
+    try {
+      const response = await axios.get(process.env.REACT_APP_SERVER_ENDPOINT+"/api/live_sensitive_transactions");
+      const newEvents = response.data;
+      if (newEvents.length > 0) {
+        holdData=[...holdData,...newEvents];
+      }
+    } catch (error) {
+      console.error('Error fetching events:', error);
+    }
+  };
+
   useEffect(() => {
+
+    fetchEventsFromApi();
+
     // Add a new event every second
-    const intervalId = setInterval(addEvent, 1000);
+    const intervalId = setInterval(fetchEventsFromApi, 5000);
+    const intervalEventAdd = setInterval(eventAdder, 1000);
 
     // Cleanup function to remove the interval when the component is unmounted
-    return () => clearInterval(intervalId);
+    return () => {
+      clearInterval(intervalId);
+      clearInterval(intervalEventAdd);
+    }
   }, []);
 
   return (
@@ -45,7 +79,7 @@ const colors = tokens(theme.palette.mode);
                 appearActive: 'event-item-move', // Handle movement of existing elements when a new element is added
               }}>
 <Box className="event-item-entered" style={{ animationDelay: `${(events.length - index) * 100}ms` }}
-              key={`${event.datetime}-${index}`}
+              key={`${event.id}`}
               display="flex"
               justifyContent="space-between"
               alignItems="center"
@@ -58,19 +92,19 @@ const colors = tokens(theme.palette.mode);
                   variant="h5"
                   fontWeight="600"
                 >
-                  google.com
+                  {event.domain}
                 </Typography>
                 <Typography color={colors.grey[100]}>
-                  godwin.pinto86@gmail.com
+                  {event.user_id}
                 </Typography>
               </Box>
-              <Box color={colors.grey[100]}>{event.datetime}</Box>
+              <Box color={colors.grey[100]}>{event.created_dt}</Box>
               <Box
                 backgroundColor={colors.greenAccent[500]}
                 p="5px 10px"
                 borderRadius="4px"
               >
-View
+{event.action=="C"?"Copy":event.action=="X"?"Cut":event.action=="P"?"Paste":event.action=="I"?"Visited":"Unknown"}
               </Box>
             </Box>
             </CSSTransition>
